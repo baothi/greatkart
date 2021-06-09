267b7f5497b106508999b87a81b4e2e858b4c112
baothi <baothi246@gmail.com>
2021-06-09 15:28:43 +0700

    add cart

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from store.models import Product
+from .models import Cart, CartItem
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -20,6 +22,35 @@ def add_cart(request, product_id):
         )
     cart.save()
 
+    try:
+        cart_item = CartItem.objects.get(product=product,cart=cart)
+        cart_item.quantity += 1 #cart_item.quantity = cart_item.quantity +1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product = product,
+            quantity = 1,
+            cart = cart,
+        )
+        cart_item.save()
+    return redirect('cart')
 
-def cart(request):
-    return render(request, 'store/cart.html')
+def cart(request,total=0, quantity=0,cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (2 * total)/100
+        grand_total = total +tax
+    except ObjectDoesNotExist:
+        pass #just ignore
+    context = {
+        'total'      : total,
+        'quantity'   : quantity,
+        'cart_items' : cart_items,
+        'tax'        : tax,
+        'grand_total': grand_total,
+    }
+    return render(request, 'store/cart.html', context)
