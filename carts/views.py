3cdb199815c449313e39b3839905f76d4a0494e7
baothi <baothi246@gmail.com>
2021-06-10 14:00:30 +0700

    search

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 
 
 # Create your views here.
@@ -35,8 +36,42 @@ def add_cart(request, product_id):
         cart_item.save()
     return redirect('cart')
 
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    try:
+        # if request.user.is_authenticated:
+        #     cart_item = CartItem.objects.get(product=product, user=request.user, id=cart_item_id)
+        # else:
+        #     cart = Cart.objects.get(cart_id=_cart_id(request))
+        #     cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+    except:
+        pass
+    return redirect('cart')
+
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    # if request.user.is_authenticated:
+    #     cart_item = CartItem.objects.get(product=product, user=request.user, id=cart_item_id)
+    # else:
+    #     cart = Cart.objects.get(cart_id=_cart_id(request))
+    #     cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
+    cart_item.delete()
+    return redirect('cart')
+
 def cart(request,total=0, quantity=0,cart_items=None):
     try:
+        tax = 0
+        grand_total = 0
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
